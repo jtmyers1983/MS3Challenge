16fb15e2763d45b5cf951f6e4f23e97fc7070242
James Myers <58834036+jtmyers1983@users.noreply.github.com>
2020-08-22 02:44:13 -0500

    implemented CSVReader, and added .csv file to project

--- src/main/java/com/jamesmyersdev/App.java
+++ src/main/java/com/jamesmyersdev/App.java
@@ -1,13 +1,25 @@
 package com.jamesmyersdev;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException, CsvValidationException {
+
+        CSVReader reader = new CSVReader(new FileReader("ms3Interview - Jr Challenge 2.csv"));
+        String [] nextLine;
+        while ((nextLine = reader.readNext()) != null) {
+            // nextLine[] is an array of values from the line
+            System.out.println(nextLine[0]);
+        }
+
     }
 }
