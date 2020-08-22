2320ba815afe6865bddce0f98831f0c85f7bbc3f
James Myers <58834036+jtmyers1983@users.noreply.github.com>
2020-08-22 18:19:08 -0500

    added ms3Interview-bad.csv, and added functionality to write incomplete records to that file

--- src/main/java/com/jamesmyersdev/App.java
+++ src/main/java/com/jamesmyersdev/App.java
@@ -1,9 +1,11 @@
 package com.jamesmyersdev;
 
 import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 import com.opencsv.exceptions.CsvValidationException;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -16,14 +18,17 @@ public class App
     public static void main( String[] args ) throws IOException, CsvValidationException {
 
         CSVReader reader = new CSVReader(new FileReader("ms3Interview.csv"));
+        CSVWriter writer = new CSVWriter(new FileWriter("ms3Interview-bad.csv"));
 
         String [] nextLine;
         nextLine = reader.readNext();
         while ((nextLine = reader.readNext()) != null) {
             if (Arrays.asList(nextLine).contains("")) {
-                System.out.println(nextLine[0]);
+                writer.writeNext(nextLine);
             }
         }
 
+        writer.close();
+        reader.close();
     }
 }
